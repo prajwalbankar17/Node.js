786a2075a24aa5c0b15fc5f0fd73c1b7ccdd403c
Prajwal <prajwalbankar11@gmail.com>
2025-08-18 21:21:44 +0530

    ESModules

---
+++ esModule/app.js
@@ -0,0 +1,11 @@
+// const mult = require('./math.js');
+
+import { add, subs, div, mult, PI } from "./math.js";
+
+console.log(add(5,10));
+console.log(mult(5,10));
+console.log(subs(5,10));
+console.log(div(5,10));
+console.log(PI);
+
+
---
+++ esModule/math.js
@@ -0,0 +1,20 @@
+const add = (a,b) => {
+    return a + b;
+};
+
+const subs = (a,b) => {
+    return a - b;
+};
+
+const div = (a,b) => {
+    return a / b;
+};
+
+const mult = (a,b) => {
+    return a * b;
+};
+
+const PI = 3.14;
+
+export  { add, subs, div, mult, PI };
+
