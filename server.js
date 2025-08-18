615aaf2fec12370ae5518ce63b6c53fdddd189d1
Prajwal <prajwalbankar11@gmail.com>
2025-08-18 20:36:46 +0530

    create an server

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const server = http.createServer((req,res) => {
+    if (req.url === '/') {
+        res.write("I am Prajwal Bankar");
+        res.end();
+    }
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+    console.log(`server is running on PORT ${PORT}`)
+})
