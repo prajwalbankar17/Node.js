e8823f11eb4d3170d97fbdd0d7e3222eacdc9bec
Prajwal <prajwalbankar11@gmail.com>
2025-08-19 11:24:05 +0530

    Syncronous, ASyncronous operations

--- behindTheScene/app.js
+++ behindTheScene/app.js
@@ -1,10 +1,17 @@
 // import fs from "fs";
 const fs = require("fs"); 
 
+//Syncronous Task
 console.log("Start");
 
+//Syncronous Task
 const data = fs.readFileSync("file.txt", "utf-8");
 console.log("data:", data);
 
+//Asynchronous Task
+fs.readFile("file.txt","utf-8", (err, data) => {
+    if (err) throw err; 
+    console.log(data);
+})
 
-
+console.log("End");
