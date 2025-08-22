b623ad73879c97de83681a32b84bddac448051b7
Prajwal <prajwalbankar11@gmail.com>
2025-08-22 14:45:52 +0530

    Environment Variable

---
+++ express/app.js
@@ -0,0 +1,34 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>About Page</h1>");
+});
+
+app.get("/contact", (req, res) => {
+  return res.send(`<div class="container">
+      <h1>URL Shortner</h1>
+      <form id="shorten-form">
+        <div>
+          <label for="url">Enter URL:</label>
+          <input type="text" id="url" name="url" required />
+        </div>
+        <div>
+          <label for="shortCode">Enter shortCode:</label>
+          <input type="text" id="shortCode" name="shortCode" required />
+        </div>
+        <button type="submit">Shorten URL</button>
+      </form>
+      <h2>Shortend URLs</h2>
+      <ul id="shortened-urls"></ul>
+    </div>`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
